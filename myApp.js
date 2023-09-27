3f1009dad301dac16d26ebf8b975630c07baa401
catherinepapad <katerinapapado.01@gmail.com>
2023-09-28 00:15:26 +0300

    Update myApp.js
    Still troubleshooting for Render

--- myApp.js
+++ myApp.js
@@ -52,9 +52,9 @@ const api = require('./server.js');
 app.use(express.static('public'));
 // app.disable('strict-transport-security');
 // app.use('/_api', api);
-// app.get("/", function (request, response) {
-  // response.sendFile(__dirname + '/views/index.html');
-// });
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
