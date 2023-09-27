8dba1a9cf1122787b5d3f190dd286d6652867642
catherinepapad <katerinapapado.01@gmail.com>
2023-09-28 00:13:01 +0300

    Troubleshooting

--- myApp.js
+++ myApp.js
@@ -50,11 +50,11 @@ const app = express();
 module.exports = app;
 const api = require('./server.js');
 app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+// app.disable('strict-transport-security');
+// app.use('/_api', api);
+// app.get("/", function (request, response) {
+  // response.sendFile(__dirname + '/views/index.html');
+// });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
