548240945622a3d26075a1b50ab6abe975e1cc8a
catherinepapad <katerinapapado.01@gmail.com>
2023-09-28 11:14:06 +0300

    Challenge 8
    Disable DNS Prefetching with helmet.dnsPrefetchControl()

--- myApp.js
+++ myApp.js
@@ -56,6 +56,14 @@ ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 
 
+// 8) Disable DNS Prefetching with helmet.dnsPrefetchControl()
+// To improve performance, most browsers prefetch DNS records for the links in a page. In that way the destination ip is already known when the user clicks on a link. 
+// This may lead to over-use of the DNS service (if you own a big website, visited by millions people…), privacy issues (one eavesdropper could infer that you are on a certain page), or page statistics alteration (some links may appear visited even if they are not). 
+// If you have high security needs you can disable DNS prefetching, at the cost of a performance penalty.
+// Use the helmet.dnsPrefetchControl() method on your server.
+app.use(helmet.dnsPrefetchControl({allow: false}));
+
+
 
 
 
