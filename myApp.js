e921478ef2176cfc96b85e221c84f935447ebb8c
catherinepapad <katerinapapado.01@gmail.com>
2023-09-28 11:04:59 +0300

    Challenge 6
    Prevent IE from Opening Untrusted HTML with helmet.ieNoOpen()

--- myApp.js
+++ myApp.js
@@ -41,6 +41,11 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 
 
+// 6) Prevent IE from Opening Untrusted HTML with helmet.ieNoOpen()
+// Some web applications will serve untrusted HTML for download. Some versions of Internet Explorer by default open those HTML files in the context of your site. This means that an untrusted HTML page could start doing bad things in the context of your pages. 
+// This middleware sets the X-Download-Options header to noopen. This will prevent IE users from executing downloads in the trusted site’s context.
+// Use the helmet.ieNoOpen() method on your server.
+app.use(helmet.ieNoOpen());
 
 
 
