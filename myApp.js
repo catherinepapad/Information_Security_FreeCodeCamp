9ffcf0e55ad890c20450032d1cbdf772e821ac77
catherinepapad <katerinapapado.01@gmail.com>
2023-09-28 10:56:08 +0300

    Challenge 3
    Mitigate the Risk of Clickjacking with helmet.frameguard()

--- myApp.js
+++ myApp.js
@@ -17,6 +17,14 @@ let helmet = require('helmet');
 app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0'}));
 
 
+// 3) Mitigate the Risk of Clickjacking with helmet.frameguard()
+// Your page could be put in a <frame> or <iframe> without your consent. This can result in clickjacking attacks, among other things.
+// Clickjacking is a technique of tricking a user into interacting with a page different from what the user thinks it is. This can be obtained by executing your page in a malicious context, by means of iframing. In that context, a hacker can put a hidden layer over your page. Hidden buttons can be used to run bad scripts. 
+// This middleware sets the X-Frame-Options header. It restricts who can put your site in a frame. It has three modes: DENY, SAMEORIGIN, and ALLOW-FROM.
+// Use helmet.frameguard() passing with the configuration object {action: 'deny'}.
+app.use(helmet.frameguard({action: 'deny'}));
+
+
 
 
 
